9638b57ae0d942c116491afc422d84434b504bba
Hanz <dalubatanhans@gmaal.com>
2024-07-20 15:23:03 +0800

    Removed hardcoded video stream HTML from room.html and moved it to streams.js, where it is dynamically generated. Added AgoraRTC client creation and local stream setup in streams.js.

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,3 +1,21 @@
 const APP_ID = 'e5d4f10b172c4d6983ff3ed563b55378'
 const  CHANNEL = 'main'
 const  TOKEN = '007eJxTYJDw45D37VUunbfesiQ85Ylqc6Lwr6h3x9/v2mux6aa02mcFhlTTFJM0Q4MkQ3OjZJMUM0sL47Q049QUUzPjJFNTY3OLWQmz0xoCGRl28wqzMjJAIIjPwpCbmJnHwAAA7aoeJQ=='
+let UID;
+
+console.log('Stream.js connected')
+
+const client = AgoraRTC.createClient({mode: 'rtc', codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    let player = `<div class="video-container" id="user-container-1">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                    <div class="video-player" id="user-${UID}"></div>
+                </div>`
+}
