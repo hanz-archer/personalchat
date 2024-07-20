48655572973888e1899cf6f70223577d919f076e
Hanz <dalubatanhans@gmaal.com>
2024-07-20 19:52:26 +0800

    Updated mainpage.html with new form and script, modified settings.py, updated streams.js with new token and functionality, and added styles to main.css

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+-#3h0*msb6^vzyprk_=z5o*xem29%7e-v25e3_mt)sp%gsx2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- static/js/streams.js
+++ static/js/streams.js
@@ -1,21 +1,52 @@
 const APP_ID = 'e5d4f10b172c4d6983ff3ed563b55378'
 const  CHANNEL = 'main'
-const  TOKEN = '007eJxTYJDw45D37VUunbfesiQ85Ylqc6Lwr6h3x9/v2mux6aa02mcFhlTTFJM0Q4MkQ3OjZJMUM0sL47Q049QUUzPjJFNTY3OLWQmz0xoCGRl28wqzMjJAIIjPwpCbmJnHwAAA7aoeJQ=='
+const  TOKEN = '007eJxTYNjzP/mcTOqH56q557s0j7H0556apnCmXuT1plcNWz9/uZ6rwJBqmmKSZmiQZGhulGySYmZpYZyWZpyaYmpmnGRqamxukTp7dlpDICPDxkuWjIwMEAjiszDkJmbmMTAAAFQdItI='
 let UID;
 
 console.log('Stream.js connected')
 
-const client = AgoraRTC.createClient({mode: 'rtc', codec:'vp8'})
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
 let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
-    let player = `<div class="video-container" id="user-container-1">
+    let player = `<div  class="video-container" id="user-container-${UID}">
                     <div class="username-wrapper"><span class="user-name">My Name</span></div>
                     <div class="video-player" id="user-${UID}"></div>
                 </div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+
+    await client.publish([localTracks[0], localTracks[1]])
 }
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if(mediaType === 'video'){
+        let player = document.getElementById('user-container-${user.uid}')
+
+        if(player != null){
+            player.remove()
+        }
+        player = `<div  class="video-container" id="user-container-${user.uid}">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                    <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
+joinAndDisplayLocalStream()
+
