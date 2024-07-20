5be2f1051d2c4f772d2fb0d2aa9881c54e774c18
Hanz <dalubatanhans@gmaal.com>
2024-07-20 20:26:37 +0800

    SweetAlerts

--- static/js/streams.js
+++ static/js/streams.js
@@ -12,6 +12,7 @@ let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
     client.on('user-published', handleUserJoined)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -30,8 +31,16 @@ let handleUserJoined = async (user, mediaType) => {
     remoteUsers[user.uid] = user
     await client.subscribe(user, mediaType)
 
+    // SweetAlert notification
+    Swal.fire({
+        title: 'User Joined',
+        text: `User ${user.uid} has joined the room`,
+        icon: 'success',
+        confirmButtonText: 'OK'
+    });
+
     if(mediaType === 'video'){
-        let player = document.getElementById('user-container-${user.uid}')
+        let player = document.getElementById(`user-container-${user.uid}`)
 
         if(player != null){
             player.remove()
